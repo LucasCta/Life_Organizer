a79b0566985cdcd858f370a026dab85fc0445f92
LucasCta <lucas.ct.araujo@gmail.com>
2023-07-21 18:05:17 -0300

    Testing features

---
+++ src/app/floating-card/floating-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FloatingCardComponent } from './floating-card.component';
+
+describe('FloatingCardComponent', () => {
+  let component: FloatingCardComponent;
+  let fixture: ComponentFixture<FloatingCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FloatingCardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FloatingCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/floating-card/floating-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDrag} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-floating-card',
+  templateUrl: './floating-card.component.html',
+  styleUrls: ['./floating-card.component.css']
+})
+export class FloatingCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
